8e34a637c5eaf51b5c2c5dee0914908d6c700c39
Gadu25 <alexanderudag@gmail.com>
2025-12-01 17:50:43 +0800

    setup progress for env

--- cmd/main.go
+++ cmd/main.go
@@ -3,10 +3,28 @@ package main
 import (
 	"log"
 	"megome/cmd/api"
+	"megome/config"
+	"megome/internal/data/db"
+
+	"github.com/go-sql-driver/mysql"
 )
 
 func main() {
-	server := api.NewAPIServer(":8080", nil)
+	db, err := db.NewMySQLStorage(mysql.Config{
+		User:                 config.Envs.DBUser,
+		Passwd:               config.Envs.DBPassword,
+		Addr:                 config.Envs.DBAddress,
+		DBName:               config.Envs.DBName,
+		Net:                  "tcp",
+		AllowNativePasswords: true,
+		ParseTime:            true,
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	server := api.NewAPIServer(":8080", db)
 	if err := server.Run(); err != nil {
 		log.Fatal((err))
 	}

87cc56fd7c0e2fe9d964c3edd3314050f6c09783
Gadu25 <alexanderudag@gmail.com>
2026-03-10 16:41:39 +0800

    fix: removed unecessary codes

--- internal/services/auth/jwt.go
+++ internal/services/auth/jwt.go
@@ -24,21 +24,6 @@ type Claims struct {
 
 const UserKey contextKey = "userID"
 
-// func CreateJWT(secret []byte, userID int) (string, error) {
-// 	expiration := time.Second * time.Duration(config.Envs.JWTExpirationInSeconds)
-// 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-// 		"userId":    strconv.Itoa(userID),
-// 		"expiresAt": time.Now().Add(expiration).Unix(),
-// 	})
-
-// 	tokenString, err := token.SignedString(secret)
-// 	if err != nil {
-// 		return "", err
-// 	}
-
-// 	return tokenString, nil
-// }
-
 func CreateJWT(secret []byte, userID int) (string, error) {
 	expiration := time.Duration(config.Envs.JWTExpirationInSeconds) * time.Second
 

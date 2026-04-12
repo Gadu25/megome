e03c765c5cecdddc66d0a1d30e03083c8f71035a
Gadu25 <alexanderudag@gmail.com>
2026-04-12 10:25:04 +0800

    fix: finalize move of fname and lname to profile table

--- internal/services/types/types.go
+++ internal/services/types/types.go
@@ -29,16 +29,12 @@ type UserStore interface {
 
 type User struct {
 	ID        int       `json:"id"`
-	FirstName string    `json:"firstName"`
-	LastName  string    `json:"lastName"`
 	Email     string    `json:"email"`
 	Password  string    `json:"password"`
 	CreatedAt time.Time `json:"createdAt"`
 }
 
 type RegisterUserPayload struct {
-	FirstName string `json:"firstName" validate:"required"`
-	LastName  string `json:"lastName" validate:"required"`
 	Email     string `json:"email" validate:"required,email"`
 	Password  string `json:"password" validate:"required,min=3,max=130"`
 }

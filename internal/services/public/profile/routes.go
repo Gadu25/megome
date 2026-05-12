525a80e9833da554ea8305db4f80b36d7a0f381c
Gadu25 <alexanderudag@gmail.com>
2026-05-12 11:29:44 +0800

    feat: add public api for profile

---
+++ internal/services/public/profile/routes.go
@@ -0,0 +1,58 @@
+package publicprofile
+
+import (
+	"fmt"
+	"megome/internal/services/auth"
+	"megome/internal/services/middleware"
+	"megome/internal/services/types"
+	"megome/internal/services/utils"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type Handler struct {
+	profileStore types.ProfileStore
+	patStore     types.PersonalAccessTokenStore
+	apiLogStore  types.APIUsageLogStore
+}
+
+type PublicResponse struct {
+	Message string         `json:"message"`
+	Data    *types.Profile `json:"data"`
+}
+
+func NewHandler(profileStore types.ProfileStore, patStore types.PersonalAccessTokenStore, apiLogStore types.APIUsageLogStore) *Handler {
+	return &Handler{
+		profileStore: profileStore,
+		patStore:     patStore,
+		apiLogStore:  apiLogStore,
+	}
+}
+
+func (h *Handler) RegisterRoutes(router *mux.Router) {
+	router.HandleFunc("/profile",
+		auth.WithPATAuth(
+			middleware.WithAPILogging(h.handleGetPublicProfile, h.apiLogStore),
+			h.patStore,
+		),
+	).Methods("GET")
+}
+
+func (h *Handler) handleGetPublicProfile(w http.ResponseWriter, r *http.Request) {
+	userID := auth.GetPATUserIDFromContext(r.Context())
+
+	profile, err := h.profileStore.GetPublicProfile(userID)
+	fmt.Println("[DEBUG] userID", userID)
+	if err != nil {
+		utils.WriteError(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	resp := PublicResponse{
+		Message: "profile successfully fetched",
+		Data:    profile,
+	}
+
+	utils.WriteJSON(w, http.StatusOK, resp)
+}

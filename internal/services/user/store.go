bc2489d1c537c6f2b8e9fee36dbba0a8a3ce7ddd
Gadu25 <alexanderudag@gmail.com>
2026-06-03 05:44:51 +0800

    feat: add store function for checking username and email on user table

--- internal/services/user/store.go
+++ internal/services/user/store.go
@@ -35,6 +35,27 @@ func (s *Store) GetUserByEmail(email string) (*types.User, error) {
 	return u, nil
 }
 
+func (s *Store) GetUserByEmailOrUsername(input string) (*types.User, error) {
+	rows, err := s.db.Query("SELECT * FROM users WHERE email = ? OR username = ?", input, input)
+	if err != nil {
+		return nil, err
+	}
+
+	u := new(types.User)
+	for rows.Next() {
+		u, err = scanRowIntoUser(rows)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	if u.ID == 0 {
+		return nil, fmt.Errorf("user not found")
+	}
+
+	return u, nil
+}
+
 func scanRowIntoUser(rows *sql.Rows) (*types.User, error) {
 	user := new(types.User)
 

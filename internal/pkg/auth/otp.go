39e22d86ef2ed441863318ad77751c5365973863
Gadu25 <alexanderudag@gmail.com>
2026-08-17 06:41:09 +0800

    feat(auth): add crypto-random 6-digit OTP generator

---
+++ internal/pkg/auth/otp.go
@@ -0,0 +1,17 @@
+package auth
+
+import (
+	"crypto/rand"
+	"fmt"
+	"math/big"
+)
+
+// GenerateOTP returns a 6-digit numeric one-time password.
+func GenerateOTP() (string, error) {
+	max := big.NewInt(1000000)
+	n, err := rand.Int(rand.Reader, max)
+	if err != nil {
+		return "", err
+	}
+	return fmt.Sprintf("%06d", n.Int64()), nil
+}
